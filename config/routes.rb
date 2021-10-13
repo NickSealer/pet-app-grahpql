8e08e1e3f06cfcff8aacdbf8a4ef7c367d2e226e
NickSealer <nickfontechav@gmail.com>
2021-10-13 13:40:36 -0500

    add home page view

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
   mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: 'graphql#execute'
+
+  root 'home#index'
   post '/graphql', to: 'graphql#execute'
 end
