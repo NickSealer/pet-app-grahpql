edff0c030afd6639113e5a94f71beff312991016
NickSealer <nickfontechav@gmail.com>
2021-10-13 12:10:24 -0500

    remove dev conditional

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@
 
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
-  mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: 'graphql#execute' if Rails.env.development?
+  mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: 'graphql#execute'
   post '/graphql', to: 'graphql#execute'
 end
