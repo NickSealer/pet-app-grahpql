12c66406d7444a21770758fcf4b15b07c13deeea
NickSealer <nickfontechav@gmail.com>
2021-10-12 20:41:56 -0500

    Update User

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -4,5 +4,6 @@ module Types
   class MutationType < Types::BaseObject
     # GraphQL resources
     field :create_user, resolver: Mutations::Users::Create
+    field :update_user, resolver: Mutations::Users::Update
   end
 end
